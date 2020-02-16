b646da5dbe78d54f273a0341206298ff8020e897
Savio Sailas <30656522+saviosailas@users.noreply.github.com>
2020-02-16 17:27:49 +0530

    Update github.sh

--- source/github.sh
+++ source/github.sh
@@ -1,31 +1,17 @@
 #!/bin/bash
 sudo apt-get -y update
-
-echo "Installing git"
 sudo apt-get install -y git
-
 
 mkdir -p ~/.ssh/
 cd ~/.ssh/
 echo "Generating SSH key"
 read -p "Enter your email used for GitHub : " email
-echo "
-     Please press Enter key when prompted to enter a FILE NAME
-     "
-echo "
-     Please don't skip password
-     "
+echo " \nPlease press Enter key when prompted to enter a FILE NAME "
+echo " \nPlease don't skip password "
 sleep 2
 ssh-keygen -t rsa -b 4096 -C $email
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
-echo "
-https://github.com/settings/ssh/new 
-
-Visit above link and copy and paste following ssh key
-"
+echo " \nhttps://github.com/settings/ssh/new \nVisit above link and copy and paste following ssh key"
 more ~/.ssh/id_rsa.pub
-echo "\n\nYour device is configured to use git from terminal"
-
-
-
+echo " \nYour device is configured to use git from terminal"
